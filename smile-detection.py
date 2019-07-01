8032ff66e009e4aefc99ee4baba5072c9efb3ae2
Matthieu Court <matthieu.court@gmail.com>
2019-06-30 23:00:54 -0400

    cleaning up the speech to text and smile detection

--- smile-detection.py
+++ smile-detection.py
@@ -7,28 +7,40 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 # Defining a function that will do the detections
-def detect(gray, frame):#gray is the greyscale version of the image the webcam will take and frame is the original image
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)#face detector
+
+#gray is the greyscale version of the image the webcam will take and frame is the original image
+def detect(gray, frame):
+    #face detector
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)#the rectangle that will surround the face which will be blue in color ( 255,0,0 -> blue )
+        #the rectangle that will surround the face which will be blue in color ( 255,0,0 -> blue )
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)#eye detector
+        #eye detector
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2) #same as face except green in color
+            #same as face except green in color
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2) 
         smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 10)
         for (sx, sy, sw, sh) in smiles:
-            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2) # red ib color that detects smile and you may need to change the value of threshold . Here , I used 50
-    return frame #we return the original frame , because we want colored output
+            # red ib color that detects smile and you may need to change the value of threshold . Currently set at 10
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2) 
+    #we return the original frame , because we want colored output        
+    return frame 
 
 # Doing some Face Recognition with the webcam
-video_capture = cv2.VideoCapture(0)# starting the webcam
+# starting the webcam
+video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#converting to grayscale.
+    #converting to grayscale.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     canvas = detect(gray, frame)
-    cv2.imshow('Video', canvas)#showing the video feed
+    #showing the video feed
+    cv2.imshow('Video', canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 video_capture.release()
-cv2.destroyAllWindows()#destroying the windows after it quits 
+#destroying the windows after it quits 
+cv2.destroyAllWindows()
--- speech2text.py
+++ speech2text.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 from os import path
 from pydub import AudioSegment
-# from pocketsphinx import AudioFile
+from pocketsphinx import AudioFile
 from SimpleAudioIndexer import SimpleAudioIndexer as sai
 import youtube_dl
 import pyaudio
@@ -22,67 +22,49 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     # throw in the url of the video audio you want to grab as an mp3 here
     ydl.download(['https://www.youtube.com/watch?v=JZRXESV3R74'])
 
+# TODO right now src is being hardcoded - be able to grab title from url in the future
 src = "Donald Trump - The Art of the Insult-JZRXESV3R74.mp3"
 dst = "test.wav"
 
-# convert wav to mp3                                                            
+# convert mp3 to wav for Sphinx speech to text                                                          
 sound = AudioSegment.from_mp3(src)
 sound.export(dst, format="wav")
 
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "test.wav")
+#other format examples
 # AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
 # AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
 
 # use the audio file as the audio source
+
 r = sr.Recognizer()
-framerate = 0.1
+framerate = 10
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)  # read the entire audio file
     decoder = r.recognize_sphinx(audio, show_all=True)
     print(decoder.seg())
-    #print(decoder)
-    # for seg in decoder.seg():
-    #     print(seg)
-    # print ([(seg.word, seg.start_frame/framerate) for seg in decoder.seg()])
-    for seg in decoder.seg():
-        print(seg.word, seg.start_frame, seg.end_frame)
+    
+    # Print out the entire transcript of the audio file
+    # ------------------
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+    # ------------------
 
-        # run a test where the word stupid gets saved as a new audio file
+    for seg in decoder.seg():
+        print(seg.word, 'start time: ', seg.start_frame, ' | end time: ', seg.end_frame)
+
+        # run a test where the word stupid gets saved as a new audio file in the current dir
 
         if (seg.word == 'stupid'):
             print('******', seg.word)
-            print('******', seg.start_frame, seg.end_frame)
-            t1 = (seg.start_frame -50) * 10
-            t2 = (seg.end_frame + 300) * 10
+            t1 = (seg.start_frame -50) * framerate
+            t2 = (seg.end_frame + 75) * framerate
 
+            # Slice and export and new audio file with t1 and t2 being the bounds
             stupidAudio = AudioSegment.from_wav(AUDIO_FILE)
             stupidAudio = stupidAudio[t1:t2]
             stupidAudio.export('newStupid.wav', format='wav') # exports a wav to the current path
-
-# recognize speech using Sphinx
-# ------------------
-# try:
-#     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-# except sr.UnknownValueError:
-#     print("Sphinx could not understand audio")
-# except sr.RequestError as e:
-#     print("Sphinx error; {0}".format(e))
-# -----------------
-
-
-# indexer = sai(mode="cmu", src_dir="C:/Users/matth/OneDrive/4A/MSCI 401/gregarious-wolf")
-# indexer.index_audio()
-# print(indexer.get_timestamps())
-
-# Frames per Second
-# fps = 100
-
-# audio1 = sr.AudioFile(AUDIO_FILE)
-
-# for phrase in audio1:  # frate (default=100)
-#     print('-' * 28)
-#     print('| %5s |  %3s  |   %4s   |' % ('start', 'end', 'word'))
-#     print('-' * 28)
-#     for s in phrase.seg():
-#         print('| %4ss | %4ss | %8s |' % (s.start_frame / fps, s.end_frame / fps, s.word))
-#     print('-' * 28)
