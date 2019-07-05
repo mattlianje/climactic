63b72bac8d8e7377b4895c03dcd6e097fe65bbf2
Tyler Lam <tylerlam888@gmail.com>
2019-07-05 15:57:42 -0400

    added round class, ability to parse rounds, and determine high rate times

--- demofile-parsing/parse_demo.js
+++ demofile-parsing/parse_demo.js
@@ -1,24 +1,47 @@
 const fs = require("fs");
 const demofile = require("demofile");
 const path = require("path");
+const Round = require('./Round.js');
 
-fs.readFile(path.resolve("/Users/tylerlam/Downloads/test.dem"), (err, buffer) => {
+const demPath = "/Users/tylerlam/Downloads/test.dem"; //add the filepath here
+
+fs.readFile(path.resolve(demPath), (err, buffer) => {
   const demoFile = new demofile.DemoFile();
+  var rounds = [];
 
+  // parse start of the round
+  demoFile.gameEvents.on("round_start", e => {
+    rounds.push(new Round(demoFile.currentTime));
+  });
+
+  // parse end of the round
+  var roundIndex = 0;
+  demoFile.gameEvents.on("round_end", e => {
+    var r = rounds[roundIndex];
+    r.endTime = demoFile.currentTime;
+    r.sortKeyEvents();
+    r.calculateEventRates();
+    r.getHighRateTimes();
+    r.mergeTimeRanges();
+    roundIndex++;
+  });
+
+  // parse deaths 
   demoFile.gameEvents.on("player_death", e => {
-    const victim = demoFile.entities.getByUserId(e.userid);
-    const victimName = victim ? victim.name : "unnamed";
+    deathEvent = { time: demoFile.currentTime, type: "player_death" };
+    rounds[roundIndex].pushKeyEvent(deathEvent);
+  });
 
-    // Attacker may have disconnected so be aware.
-    // e.g. attacker could have thrown a grenade, disconnected, then that grenade
-    // killed another player.
-    const attacker = demoFile.entities.getByUserId(e.attacker);
-    const attackerName = attacker ? attacker.name : "unnamed";
+  // parse bomb_defuses
+  demoFile.gameEvents.on("bomb_defused", e => {
+    bombDefuseEvent = { time: demoFile.currentTime, type: "bomb_defused" };
+    rounds[roundIndex].pushKeyEvent(bombDefuseEvent);
+  });
 
-    const headshotText = e.headshot ? " HS" : "";
-    const currentTime = demoFile.currentTime;
-
-    console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName} -> time: ${currentTime}`);
+  // parse bomb_planted
+  demoFile.gameEvents.on("bomb_planted", e => {
+    bombPlantEvent = { time: demoFile.currentTime, type: "bomb_planted" };
+    rounds[roundIndex].pushKeyEvent(bombPlantEvent);
   });
 
   demoFile.parse(buffer);
