4d2bb7bee335bed90e8a8790016c76f6f4258fd7
Tyler Lam <tylerlam888@gmail.com>
2019-07-07 17:02:21 -0400

    added logging

--- demofile-parsing/parse_demo.js
+++ demofile-parsing/parse_demo.js
@@ -2,8 +2,33 @@ const fs = require("fs");
 const demofile = require("demofile");
 const path = require("path");
 
-fs.readFile(path.resolve("/Users/tylerlam/Downloads/test.dem"), (err, buffer) => {
+const demPath = "/Users/tylerlam/Downloads/exec-vs-noname-mirage.dem"; //add the filepath here
+const gameStart = 13301; 
+
+fs.readFile(path.resolve(demPath), (err, buffer) => {
   const demoFile = new demofile.DemoFile();
+  const rounds = [];
+  // parse start of the round
+  demoFile.gameEvents.on("round_start", e => {
+    console.log(`round ${roundIndex} started ${demoFile.currentTime}`);
+    // rounds.push(new Round(demoFile.currentTime));
+  });
+
+  // parse end of the round
+  var roundIndex = 0;
+  demoFile.gameEvents.on("round_end", e => {
+    // var r = rounds[roundIndex];
+    // r.endTime = demoFile.currentTime;
+    // r.sortKeyEvents();
+    // r.calculateEventRates();
+    // r.getHighRateTimes();
+    // r.mergeTimeRanges();
+    // r.mapToStreamTimestamps(gameStart);
+    // r.plotAllEvents();
+    // r.plotHighRates();
+    console.log(`round ${roundIndex} ended`);
+    roundIndex++;
+  });
 
   demoFile.gameEvents.on("player_death", e => {
     const victim = demoFile.entities.getByUserId(e.userid);
@@ -20,6 +45,18 @@ fs.readFile(path.resolve("/Users/tylerlam/Downloads/test.dem"), (err, buffer) =>
 
     console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName} -> time: ${currentTime}`);
   });
+
+  // // parse bomb_defuses
+  // demoFile.gameEvents.on("bomb_defused", e => {
+  //   bombDefuseEvent = { time: demoFile.currentTime, type: "bomb_defused" };
+  //   rounds[roundIndex].pushKeyEvent(bombDefuseEvent);
+  // });
+
+  // // parse bomb_planted
+  // demoFile.gameEvents.on("bomb_planted", e => {
+  //   bombPlantEvent = { time: demoFile.currentTime, type: "bomb_planted" };
+  //   rounds[roundIndex].pushKeyEvent(bombPlantEvent);
+  // });
 
   demoFile.parse(buffer);
 });
