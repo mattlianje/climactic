9e477941c4a9813e972bfba060f41644f8b3074c
Eldrick19 <eldrickman19@gmail.com>
2019-07-07 21:16:54 -0400

    Demo Parsing Debugging
    Fixes Demo Parsing Errors by catching the following:
    
    1) Discards any event happening before a given round (therefore includes events between rounds too)
    
    2) Discards any event happening before a round END if it doesn't have a round START
    
    3) Discards duplicate round STARTS

---
+++ demofile-parsing/Round.js
@@ -0,0 +1,99 @@
+const demofile = require("demofile");
+
+class Round {
+
+  constructor (startTime, endTime, keyEvents) {
+    this.startTime = startTime || null;
+    this.endTime = endTime || null;
+    this.keyEvents = keyEvents || [];
+    this.eventRates = [];
+    this.highRateTimes = [];
+    this.highlights = [];
+  }
+
+  // check if a time is within the round
+  inRound(time) {
+    return this.startTime <= time && this.endTime >= time;
+  }
+
+  pushKeyEvent(keyEvent) {
+    this.keyEvents.push(keyEvent);
+  }
+
+  sortKeyEvents() {
+    this.keyEvents.sort(this.compare);
+  }
+
+  compare( a, b ) {
+    if ( a.time < b.time ){
+      return -1;
+    }
+    if ( a.time > b.time ){
+      return 1;
+    }
+    return 0;
+  }
+
+  calculateEventRates(roundIndex) {
+    var i;
+    for (i = 0; i+1 < this.keyEvents.length; i++) {
+      var deltaTime = this.keyEvents[i+1].time - this.keyEvents[i].time;
+      if (deltaTime == 0) { continue; }
+      var rate = 1/deltaTime;
+      this.eventRates.push(rate);
+    }
+    console.log(`==== Round ${roundIndex}`);
+  }
+
+  getHighRateTimes() {
+    // find average rate
+    var total = 0;
+
+    for(var i = 0; i < this.eventRates.length; i++) {
+        total += this.eventRates[i];
+    }
+    var avg = total / this.eventRates.length;
+
+    var times = [];
+    // const highlightLength = 10;
+    for(var j = 0; j < this.eventRates.length; j++) {
+      if (this.eventRates[j] > avg) {
+        var start = this.keyEvents[j].time - 2;
+        var end = this.keyEvents[j+1].time + 2;
+        this.highRateTimes.push([start, end]);
+      }
+    }
+    console.log("High-Rate Times (Before Merge):");
+    console.log(this.highRateTimes); 
+  }
+
+  mergeTimeRanges() {
+    var i = 0;
+    var j = 1;
+    var tmp = [this.highRateTimes[i]];
+    while (j < this.highRateTimes.length) {
+      var rangeA = tmp[i];
+      var rangeB = this.highRateTimes[j];
+      if (this.timesOverlap(rangeA, rangeB)) {
+        tmp[i][0] = Math.min(rangeA[0], rangeB[0]);
+        tmp[i][1] = Math.max(rangeA[1], rangeB[1]);
+        j++;
+      } else {
+        tmp.push(rangeB);
+        i++;
+        j++;
+      }
+    }
+    this.highlights = tmp;
+    console.log("Final Round Highlights:");
+    console.log(this.highlights);
+  }
+
+  timesOverlap(timeA, timeB) {
+    var [startA, endA] = timeA;
+    var [startB, endB] = timeB;
+    return ((startB <= endA) || (endA >= startB));
+  }
+}
+
+module.exports = Round;
--- demofile-parsing/parse_demo.js
+++ demofile-parsing/parse_demo.js
@@ -1,24 +1,81 @@
 const fs = require("fs");
 const demofile = require("demofile");
 const path = require("path");
+const Round = require("./Round.js");
 
 fs.readFile(path.resolve("/Users/tylerlam/Downloads/test.dem"), (err, buffer) => {
   const demoFile = new demofile.DemoFile();
+  const rounds = [];
+  var round_i = false;
+
+  // parse start of the round
+  demoFile.gameEvents.on("round_start", e => {
+    if(round_i == true){
+      rounds.pop();
+    }
+    round_i = true;
+    rounds.push(new Round(demoFile.currentTime));
+    
+    // CONSOLE LOGGING - ONLY NECESSARY FOR TEST PURPOSES
+    // console.log(`round ${roundIndex} started ${demoFile.currentTime}`);
+  });
+
+  // parse end of the round
+  var roundIndex = 0;
+  demoFile.gameEvents.on("round_end", e => {
+    if(round_i == true){
+      var r = rounds[roundIndex];
+      r.endTime = demoFile.currentTime;
+      r.sortKeyEvents();
+      r.calculateEventRates(roundIndex);
+      r.getHighRateTimes();
+      r.mergeTimeRanges();
+      // r.mapToStreamTimestamps(gameStart);
+      // r.plotAllEvents();
+      // r.plotHighRates();
+
+      // CONSOLE LOGGING - ONLY NECESSARY FOR TEST PURPOSES
+      // console.log(`round ${roundIndex} ended`);
+      round_i == false;
+      roundIndex++;
+    }
+  });
 
   demoFile.gameEvents.on("player_death", e => {
-    const victim = demoFile.entities.getByUserId(e.userid);
-    const victimName = victim ? victim.name : "unnamed";
+    if(round_i == true){
+      deathEvent = { time: demoFile.currentTime, type: "player_death" };
+      rounds[roundIndex].pushKeyEvent(deathEvent);
 
-    // Attacker may have disconnected so be aware.
-    // e.g. attacker could have thrown a grenade, disconnected, then that grenade
-    // killed another player.
-    const attacker = demoFile.entities.getByUserId(e.attacker);
-    const attackerName = attacker ? attacker.name : "unnamed";
+      // CONSOLE LOGGING - ONLY NECESSARY FOR TEST PURPOSES
+      // const victim = demoFile.entities.getByUserId(e.userid);
+      // const victimName = victim ? victim.name : "unnamed";
 
-    const headshotText = e.headshot ? " HS" : "";
-    const currentTime = demoFile.currentTime;
+      // // Attacker may have disconnected so be aware.
+      // // e.g. attacker could have thrown a grenade, disconnected, then that grenade
+      // // killed another player.
+      // const attacker = demoFile.entities.getByUserId(e.attacker);
+      // const attackerName = attacker ? attacker.name : "unnamed";
 
-    console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName} -> time: ${currentTime}`);
+      // const headshotText = e.headshot ? " HS" : "";
+      // const currentTime = demoFile.currentTime;
+      // console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName} -> time: ${currentTime}`);
+    }
+  });
+
+  // parse bomb_defuses
+  demoFile.gameEvents.on("bomb_defused", e => {
+    if(round_i == true){
+      bombDefuseEvent = { time: demoFile.currentTime, type: "bomb_defused" };
+      rounds[roundIndex].pushKeyEvent(bombDefuseEvent);
+    }
+  });
+
+  // parse bomb_planted
+  demoFile.gameEvents.on("bomb_planted", e => {
+    if(round_i == true){
+      bombPlantEvent = { time: demoFile.currentTime, type: "bomb_planted" };
+      rounds[roundIndex].pushKeyEvent(bombPlantEvent);
+    }
   });
 
   demoFile.parse(buffer);
