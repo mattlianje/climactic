db63c399eb378ecde8829a8df6c387dcd89b3991
Tyler Lam <tylerlam888@gmail.com>
2020-01-11 15:53:46 -0500

    add constants file

---
+++ demofile-parsing/constants.js
@@ -0,0 +1,6 @@
+const TEST_URL = "https://www.twitch.tv/videos/449439967"; // Change according to Testing needs
+const TEST_FILE_PATH = "C:/Users/eldri/Documents/GitHub/Local_Files/heroic-vs-ence-mirage.dem"; // Change according to Testing needs
+const TEST_START_TIME = 2000; // Change according to Testing needs
+const TESTING = false;
+
+export { TEST_URL, TEST_FILE_PATH, TEST_START_TIME, TESTING };
