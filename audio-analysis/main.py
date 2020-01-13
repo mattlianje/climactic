6653db325808beee25c7dae001a1aa0b3d2ef9b5
Eldrick19 <eldrickman19@gmail.com>
2020-01-13 00:35:33 -0500

    Added test URL option, slight debugging

--- audio-analysis/main.py
+++ audio-analysis/main.py
@@ -4,38 +4,39 @@ import pandas as pd
 # Save Video function creates a video object and outputs all the nice analysis stuff
 # Will add a function to consolidate video data into a dictionary 
 # Will add a function to push video dictionaries to SQL table
-def saveVideo(ytURL, hTag):
-    videoX = videoHelper.videoObject(ytURL, hTag)
-    print(videoX.getFilename())
-    videoX.getAudio()
-    videoX.getTextAnalysis()
-    videoX.getAmplitudeAnalysis()
+def saveVideo(url, tag):
+    video = videoHelper.videoObject(url, tag)
+    print(video.getFilename())
+    video.getAudio()
+    video.getTextAnalysis()
+    video.getAmplitudeAnalysis()
 
 ##### Prompt for Highlight Video and Tagging #####
-answer = input("Are you entering a single video (enter 'single') or CSV (enter 'csv') \n")
+answer = input("Option 1. single | Option 2. csv | Option 3. test \n")
 
 #If user wants to upload only a single video
-if answer == 'single':
-    ytURL = input("Provide YT Link: ")
-    hTag = input("Provide highlight tag (True/False): ")
-    print("Link: ", ytURL)
-    print("Tag: ", hTag)
-    saveVideo(ytURL, hTag) #Call 
+if answer == 'single' or 1:
+    url = input("Provide YT Link: ")
+    tag = input("Provide highlight tag (True/False): ")
+    print("Link: ", url)
+    print("Tag: ", tag)
+    saveVideo(url, tag)
 #If user wants to upload only a CSV of videos
-elif answer == 'csv':
+elif answer == 'csv' or 2:
     inputCSV = input("Provide csv with file and path \n")
     tuplesDF = pd.read_csv(inputCSV)
     print(tuplesDF.head())
     i = 0
     while i < len(tuplesDF.index):
         print("\n For Video #", i+1, ":")
-        ytURL = tuplesDF.loc[i, 'YT_LINK']
-        hTag = tuplesDF.loc[i, 'H_TAG']
-        print("Link: ", ytURL, " | Tag: ", hTag)
-        saveVideo(ytURL, hTag)
+        url = tuplesDF.loc[i, 'YT_LINK']
+        tag = tuplesDF.loc[i, 'H_TAG']
+        print("Link: ", url, " | Tag: ", tag)
+        saveVideo(url, tag)
         i += 1
         print("\n --------------------------------")
-
+elif answer == 'test' or 3:
+    saveVideo("https://www.youtube.com/watch?v=JZRXESV3R74", True)
 
 
 
