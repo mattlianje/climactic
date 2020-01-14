69858bf36befd61d47341d7e77a44aa6d38fdcaa
Eldrick Wega <eldrickman19@gmail.com>
2020-01-13 19:04:51 -0500

    Audio list - Populated amplitude_list for video object (#18)
    * Populated amplitude_list in Video Object
    
    * Updated with Master
    
    * Clean up
    
    * Cleanup part 2
    
    * Debugging and Cleanup
    
    Adding TESTING variable on main.py and videoHelper.py
    
    Deleted all those WAV files

--- audio-analysis/main.py
+++ audio-analysis/main.py
@@ -1,6 +1,8 @@
 import videoHelper
 import pandas as pd
 
+TESTING = False #Define if you are testing or not. Make sure to check videoHelper.py for testing too
+
 # Save Video function creates a video object and outputs all the nice analysis stuff
 # Will add a function to consolidate video data into a dictionary 
 # Will add a function to push video dictionaries to SQL table
@@ -11,18 +13,25 @@ def saveVideo(url, tag):
     video.getTextAnalysis()
     video.getAmplitudeAnalysis()
 
+    if (TESTING):
+        df1 = pd.DataFrame(video.word_list) 
+        df2 = pd.DataFrame(video.amplitude_list) 
+        print(df1)
+        print(df2)
+    
+
 ##### Prompt for Highlight Video and Tagging #####
 answer = input("Option 1. single | Option 2. csv | Option 3. test \n")
 
 #If user wants to upload only a single video
-if answer == 'single' or 1:
+if answer == 'single' or answer == '1':
     url = input("Provide YT Link: ")
     tag = input("Provide highlight tag (True/False): ")
     print("Link: ", url)
     print("Tag: ", tag)
     saveVideo(url, tag)
 #If user wants to upload only a CSV of videos
-elif answer == 'csv' or 2:
+elif answer == 'csv' or answer == '2':
     inputCSV = input("Provide csv with file and path \n")
     tuplesDF = pd.read_csv(inputCSV)
     print(tuplesDF.head())
@@ -35,5 +44,5 @@ elif answer == 'csv' or 2:
         saveVideo(url, tag)
         i += 1
         print("\n --------------------------------")
-elif answer == 'test' or 3:
+elif answer == 'test' or answer == '3':
     saveVideo("https://www.youtube.com/watch?v=JZRXESV3R74", True)
