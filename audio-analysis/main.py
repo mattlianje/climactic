17db31e35785c7b3715a0d44e437e512b3e7e457
Eldrick Wega <eldrickman19@gmail.com>
2020-01-22 22:10:45 -0500

    Fleshed out SQL connection architecture (#22)
    * Fleshed out SQL connection architecture
    
    - Created sqlConnectionSetup function that checks if 'test_table' exists and creates it if it doesn't. Creates it with an auto-incremented primary key
    
    - Created urlExists function to check if URL is already in test_table.
    
    * Changes to clean up Code
    
    - Shortened code to be a little slicker

--- audio-analysis/main.py
+++ audio-analysis/main.py
@@ -1,15 +1,18 @@
 import videoHelper
+from sqlConnection import sqlConnectionSetup, urlExists, engine
 import pandas as pd
 import sys
 import lib.etl.dfHelper as dfHelper
 import pymysql
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, Numeric
 
 # Pass `python main.py log` as optional arg to see print statements
 TESTING = False
 if len(sys.argv) == 2:
     if sys.argv[1] == 'log':
         TESTING = True #Define if you are testing or not. Make sure to check videoHelper.py for testing too
+# Using this Trump Video for testing
+trump_video = "https://www.youtube.com/watch?v=JZRXESV3R74"
 
 # analyzeVideoSound creates a video object and outputs all the nice analysis stuff ...
 # inputs:
@@ -18,8 +21,9 @@ if len(sys.argv) == 2:
 #
 # outputs:
 # Summary statistics wip ...
-
+ 
 def analyzeVideoSound(url, tag):
+    global engine
     video = videoHelper.videoObject(url, tag, TESTING)
     print(video.getFilename())
     video.getAudio()
@@ -39,39 +43,55 @@ def analyzeVideoSound(url, tag):
         else:
             print('Join failed ... each word does not have an amplitude')
         print(df3)
+        #Export video breakdown to CSV
         df3.to_csv("data_export.csv", header=True)
-
-        engine = create_engine('mysql+pymysql://root:root@localhost/climactic_test')
-        df3.to_sql("test_table", con=engine, if_exists='replace')
-
-
-    
+        #Export video breakdown to SQL
+        df3.to_sql("test_table", con=engine, if_exists='append')
 
 ##### Prompt for Highlight Video and Tagging #####
-answer = input("Option 1. single | Option 2. csv | Option 3. test \n")
+def prompt():
+    answer = input("Option 1. single | Option 2. csv | Option 3. test \n")
 
-#If user wants to upload only a single video
-if answer == 'single' or answer == '1':
-    url = input("Provide YT Link: ")
-    tag = input("Provide highlight tag (True/False): ")
-    print("Link: ", url)
-    print("Tag: ", tag)
-    analyzeVideoSound(url, tag)
+    #If user wants to upload only a single video
+    if answer == 'single' or answer == '1':
+        url = input("Provide YT Link: ")
+        tag = input("Provide highlight tag (True/False): ")
+        print("Link: ", url)
+        print("Tag: ", tag)
+        #Check if video already exists
+        url_exists = urlExists(url)
+        if url_exists == True:
+            print("This video is already in the database! Did not export.")
+        else:
+            analyzeVideoSound(url, tag)
 
-#If user wants to upload only a CSV of videos
-elif answer == 'csv' or answer == '2':
-    inputCSV = input("Provide csv with file and path \n")
-    tuplesDF = pd.read_csv(inputCSV)
-    print(tuplesDF.head())
-    i = 0
-    while i < len(tuplesDF.index):
-        print("\n For Video #", i+1, ":")
-        url = tuplesDF.loc[i, 'YT_LINK']
-        tag = tuplesDF.loc[i, 'H_TAG']
-        print("Link: ", url, " | Tag: ", tag)
-        analyzeVideoSound(url, tag)
-        i += 1
-        print("\n --------------------------------")
+    #If user wants to upload only a CSV of videos
+    elif answer == 'csv' or answer == '2':
+        inputCSV = input("Provide csv with file and path \n")
+        tuplesDF = pd.read_csv(inputCSV)
+        print(tuplesDF.head())
+        i = 0
+        while i < len(tuplesDF.index):
+            print("\n For Video #", i+1, ":")
+            url = tuplesDF.loc[i, 'YT_LINK']
+            tag = tuplesDF.loc[i, 'H_TAG']
+            print("Link: ", url, " | Tag: ", tag)
+            #Check if video already exists
+            url_exists = urlExists(url)
+            if url_exists == True:
+                print("This video is already in the database! Did not export.")
+            else:
+                analyzeVideoSound(url, tag)
+            i += 1
+            print("\n --------------------------------")
 
-elif answer == 'test' or answer == '3':
-    analyzeVideoSound("https://www.youtube.com/watch?v=JZRXESV3R74", True)
+    elif answer == 'test' or answer == '3':
+        #Check if video already exists
+        url_exists = urlExists(trump_video)
+        if url_exists == True:
+            print("This video is already in the database! Did not export.")
+        else:
+            analyzeVideoSound(trump_video, True)
+
+sqlConnectionSetup()
+prompt()
