3f089df5b59cd2b41d6391500550b062d31b6cd7
Tyler Lam <tylerlam888@gmail.com>
2020-01-25 20:52:16 -0500

    Splice clips into a full highlight video (#19)
    * added video splice script, output all timestamps from demofile parsing
    
    * added comment
    
    * minor change
    
    * bug fix

---
+++ video-output/generate_video.py
@@ -0,0 +1,23 @@
+from moviepy.editor import *
+import json
+
+pathToFullVideo = "/Users/tylerlam/Downloads/CSGO - Liquid vs. mousesports [Cache] Map 1 - GRAND FINAL - ESL One New York 2018.mp4"
+
+with open("../demofile-parsing/timestamps.json", "r") as read_file:
+    data = json.load(read_file)
+
+fullvideo = VideoFileClip(pathToFullVideo)
+
+clips = []
+roundNumber = 1
+for round in data:
+  transition = TextClip("Round {}".format(roundNumber), font='Arial', fontsize=70, color="white", size=fullvideo.size)
+  roundNumber += 1
+  clips.append(transition.set_duration(3))
+  for highlight in round:
+    start_time = highlight[0]
+    end_time = highlight[1]
+    clips.append(fullvideo.subclip(start_time, end_time))
+
+final_video = concatenate_videoclips(clips)
+final_video.write_videofile("highlights.mp4", codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
