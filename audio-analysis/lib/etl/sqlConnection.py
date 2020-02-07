318a64a1c6542a857e458aa47094d01eb3afad0e
Eldrick19 <eldrickman19@gmail.com>
2020-02-07 17:38:15 -0500

    Send MFCC Dataframe to SQL Database
    - Added the generation of an MFCC table
    - Created tableExists() function to check if a table already exists
    - Created createTable() function to create table

--- audio-analysis/lib/etl/sqlConnection.py
+++ audio-analysis/lib/etl/sqlConnection.py
@@ -33,19 +33,36 @@ def getEngine(isTest):
 
 #SQL Connection Setup - Checks if table exists and if URL to be input already exists
 def sqlConnectionSetup(engine):
-    table_exists = False #Variable to check if table exists
-    # Create a metadata instance
-    meta = MetaData(engine)
-    #meta.reflect(bind=engine)
+    table_exists = False # Variable to check if table exists
+    meta = MetaData(engine) # Create a metadata instance
+    tables = ['test_table', 'mfcc_table']
+    
+    for _t in tables:
+        if tableExists(_t, meta) == False:
+            print('Table "', _t,'" does not exist. Creating..')
+            createTable(_t, meta, engine)
+    
+    meta.create_all() # Create all tables
 
+#Function checks if url exists in table already
+def urlExists(url, engine):
+    # Check if URL already exists
+    result = engine.execute('SELECT * FROM `test_table` WHERE `url`="' + url + '"')
+    print("Number of rows in database for this video", result.rowcount)
+
+    # If URL does exist return True
+    return result.rowcount > 0
+
+def tableExists(table_name, meta):
     # Check if table already exists
     for _t in meta.tables:
-        if _t == 'test_table': return
+        if _t == table_name: return True
+    return False
 
-    # If table doesn't exist, create it
-    if table_exists == False:
-        # Declare a table
-        table = Table('test_table',meta,
+def createTable(table_name, meta, engine):
+    #Creates the Test Table
+    if table_name == 'test_table':
+        test_table = Table(table_name,meta,
                     Column('id',Integer, primary_key=True, autoincrement=True),
                     Column('index',Integer),
                     Column('end_time_s',Integer),
@@ -60,17 +77,19 @@ def sqlConnectionSetup(engine):
                     Column('pitch',Float),
                     Column('p_confidence',Float),
                 )
-        # Create all tables
-        meta.create_all()
+    elif table_name == 'mfcc_test_table':
+        mfcc_table = Table(table_name,meta,
+                    Column('mfcc_id',Integer, primary_key=True, autoincrement=True),
+                    Column('index',Integer),
+                    Column('end_time_s',Integer),
+                    Column('start_time_s',Integer),
+                    Column('url',String(length=500)),
+                )
+        for i in range (1,60):
+            engine.execute('ALTER TABLE `'+ table_name +'` ADD mfcc_'+ '{0:0=2d}'.format(i) +' float;')
+    else:
+        print('ERROR: There is not setup for table "', table_name, '". Please create its setup.')
 
-#Function checks if url exists in table already
-def urlExists(url, engine):
-    # Check if URL already exists
-    result = engine.execute('SELECT * FROM `test_table` WHERE `url`="' + url + '"')
-    print("Number of rows in database for this video", result.rowcount)
-
-    # If URL does exist return True
-    return result.rowcount > 0
 
 def getTableAsDf(table_name):
     engine = getEngine(False)
