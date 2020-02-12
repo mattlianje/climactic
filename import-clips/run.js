8b8103a16dee4a25965c92bd72ba1661e42a816e
Tyler Lam <tylerlam888@gmail.com>
2020-02-12 15:44:34 -0500

    updated endpoint address

--- import-clips/run.js
+++ import-clips/run.js
@@ -46,7 +46,7 @@ const formatRow = ( youtubeUrl, interval)  => {
 }
 
 const sendToDB = (rows) => {
-  request.post('http://127.0.0.1:5000/addClips', {
+  request.post('http://data-labelling-fydp.herokuapp.com/addClips', {
     json: {
       items: rows
     }
