17ba9c1b372e5eef765c560e14eff3416fb8b0dd
Tyler Lam <tylerlam888@gmail.com>
2020-02-12 02:08:48 -0500

    revert some stuf

--- flask-app/secrets.py
+++ flask-app/secrets.py
@@ -1,9 +1,4 @@
-# user = 'admin'
-# password = 'password'
-# db = 'climactic_test'
-# host = 'climactic-test.cmikkru8vljn.us-east-1.rds.amazonaws.com'
-
 user = 'admin'
 password = 'password'
 db = 'climactic_test'
-host = 'database-1.czll7seriyxp.us-west-2.rds.amazonaws.com'
+host = 'climactic-test.cmikkru8vljn.us-east-1.rds.amazonaws.com'
