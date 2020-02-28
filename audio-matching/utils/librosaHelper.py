a09ec101179ac93a8874772e96d273ddc613872e
Tyler Lam <tylerlam888@gmail.com>
2020-02-27 23:34:26 -0500

    added everything to match audio files
    minor change
    
    deleted file that might be too big

---
+++ audio-matching/utils/librosaHelper.py
@@ -0,0 +1,44 @@
+import librosa
+import numpy as np
+from numpy import save
+from numpy import load
+from sklearn import preprocessing
+from scipy import signal
+from scipy import stats
+import os.path
+
+# retrieve librosa file from local
+def retrieveLibrosa(filePath):
+  data = load(filePath)
+  sampleRate = int(data[-1])
+  return (data[:-1], sampleRate)
+
+# convert audio file to librosa file and save it
+def createAndSaveLibrosa(audioFilePath, targetPath):
+  audio, sampleRate = librosa.load(audioFilePath)
+  # save librosa
+  save(targetPath, np.append(audio, sampleRate))
+  return (audio, sampleRate)
+
+def clipExistsInFull(clip, full):
+  # normalize
+  fullNormal = preprocessing.scale(full)
+  clipNormal = preprocessing.scale(clip)
+  # run cros-correlation to find where clip occurs in full video
+  correlation = signal.fftconvolve(fullNormal, clipNormal[::-1], mode='same')
+  # get index of max
+  idx = np.argmax(correlation)
+  # check for any spikes in correlation array
+  z = np.abs(stats.zscore(correlation))
+  spikes = np.where(z > 80) # 80 is a made up threshold
+  hasMatch = np.isin(idx, spikes)
+  return (hasMatch, idx)
+
+# check if librosa file already exists, otherwise convert
+def extractLibrosa(audioFilePath, savePath):
+  if os.path.isfile(savePath):
+    audio, sampleRate = retrieveLibrosa(savePath)
+  else: # convert audio file to librosa
+    audio, sampleRate = createAndSaveLibrosa(audioFilePath, savePath)
+  return (audio, sampleRate)
+
