5de5d63faa443dc9239b8ec84d3055ae92aa838c
Eldrick19 <eldrickman19@gmail.com>
2020-03-26 12:57:48 -0400

    Tweaks and Changes based on PR feedback

--- post-processing/getHighlightTimestamps.py
+++ post-processing/getHighlightTimestamps.py
@@ -15,12 +15,13 @@ url = 'https://www.youtube.com/watch?v=-5NzaAt_wY0' # We would get this as a var
 db_conn_str = "mysql+pymysql://{:}:{:}@{:}/{:}".format(secrets.user, secrets.password, secrets.host, secrets.db)
 db_conn = create_engine(db_conn_str)
 df = pd.read_sql('SELECT * FROM predictions_table WHERE url="'+ url +'"', con=db_conn)
+db_conn.dispose()
 
 # Loop through DFs and send highlight timestamps to arrays
 rf_timestamps, nn_timestamps = [], []
 highlight_detected_rf, highlight_detected_nn = False, False
 
-for index, row in df.iterrows():
+for i, (index, row) in enumerate(df.iterrows()): # Here i is rowcount, we use it to know when we're on the last row
     # Get Highlights from RF predictions
     if row['pred_highlight_rf'] == 1:
         if highlight_detected_rf == False: 
@@ -29,7 +30,8 @@ for index, row in df.iterrows():
             highlight_end_rf = row['end']
         else:
             highlight_end_rf = row['end']
-    else:
+
+    if row['pred_highlight_rf'] == 0 or i >= len(df) - 1:
         if highlight_detected_rf == True: 
             highlight_detected_rf = False
             rf_timestamps.append([highlight_start_rf, highlight_end_rf])
@@ -42,12 +44,15 @@ for index, row in df.iterrows():
             highlight_end_nn = row['end']
         else:
             highlight_end_nn = row['end']
-    else:
+    if row['pred_highlight_nn'] == 0 or i >= len(df) - 1:
         if highlight_detected_nn == True: 
             highlight_detected_nn = False
             nn_timestamps.append([highlight_start_nn, highlight_end_nn])
 
+# Save the resulting Numpy Arrays to a .npz file
+npzfilename = 'timestamps-arrays/' + url.split("v=",1)[1] + '-highlights-arrays.npz'
+np.savez(npzfilename, rf_timestamps=rf_timestamps, nn_timestamps=nn_timestamps)
 
-# Call the the following variables for a list of list of highlight timestamps (depending on the model)
-rf_timestamps
-nn_timestamps
+# To access the arrays:
+# npzfile = np.load('highlights-arrays.npz')
+# npzfile['rf_timestamps']
