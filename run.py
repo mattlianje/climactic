5d324c1861133689ac85c131518b557179abc621
Eldrick19 <eldrickman19@gmail.com>
2020-03-31 16:55:55 -0400

    Tweaks and Changes

--- run.py
+++ run.py
@@ -77,9 +77,7 @@ df['pitch'] = pitchVals
 
 
 # Run Post Processing (this saves highlight-timestamps arrays to the datastore)
-highlight_predictions_df = df[['start', 'end']]
-highlight_predictions_df['pred_highlight_rf'] = rf_predictions
-highlight_predictions_df['pred_highlight_nn'] = nn_predictions
-postProcessing.getHighlightTimestamps(highlight_predictions_df, vidId)
+highlight_pred_df = df[['start', 'end', 'pred_highlight_rf', 'pred_highlight_nn']]
+postProcessing.getHighlightTimestamps(highlight_pred_df, vidId)
 
 # TODO: update db with updated dataframe
